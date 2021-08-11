07afd243d8ee812c0baf62da630a63a97d71c793
GitHub Action <action@github.com>
2021-08-11 05:17:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/parvezmrobin/pyvartype/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/parvezmrobin/pyvartype/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.parvezmrobin.pyvartype.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.parvezmrobin.pyvartype.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/parvezmrobin/pyvartype/services/MyProjectService.kt
+++ src/main/kotlin/com/github/parvezmrobin/pyvartype/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.parvezmrobin.pyvartype.services
 
+import com.github.parvezmrobin.pyvartype.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
